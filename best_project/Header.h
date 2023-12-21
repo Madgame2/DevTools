08d408625a9bf2eaa2a6ed404fbedb2d3bf510b5
Anton <astakheyko@bk.ru>
2023-12-21 22:38:06 +0300

    ДОБАВЛЕННО    * добавленно и настроенно окно для  ПК/ОК/ДК ИСПРАВЛЕННО    * исправленна ошибка в библиотеке Button.h        из-за которой размер кнопки устанавливался как 0

--- best_project/Header.h
+++ best_project/Header.h
@@ -38,6 +38,11 @@ void binary_logic(int operation_ID,String label_input); //связуюшее звенно между
 
 String arithmetic_operations(String operation); //арифметические операции с точки зрения микропроцессора/микроконтролера
 
-String convertor_to_machine_code(float value); //преобразует десятичное число в машинный код
+String convertor_to_machine_code(String data, int operation_id); //преобразует десятичное число в машинный код
+																 /*
+																  * 1 - ПК
+																  * 2 - ОК
+																  * 3 - ДК
+																 */
 
 bool checking(int module_ID, String data);
--- best_project/convertor_to_machine_code.cpp
+++ best_project/convertor_to_machine_code.cpp
@@ -1,6 +1,6 @@
 #include"Header.h"
 
-String convertor_to_machine_code(float value)
+String convertor_to_machine_code(String data, int operation_id)
 {
-	return "";
+	return data;
 }
